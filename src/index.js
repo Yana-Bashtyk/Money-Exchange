bdd159ee6e45119cb708ae0019caa2bb7ef0856a
Yana Bashtyk <Yana_bashtyk@epam.com>
2018-09-29 21:05:54 +0300

    solution new with style changes

--- src/index.js
+++ src/index.js
@@ -1,41 +1,33 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-    var objExchange = {};
+    const objExchange = {};
     if (currency > 10000){
         objExchange.error = "You are rich, my friend! We don't have so much coins for exchange";
         return objExchange;
     }
-    if (currency == 0) {
+    if (currency <= 0) {
         return objExchange;
     }
-    var countH = 0, countQ = 0, countD = 0, countN = 0, countP = 0;
-    while (currency >= 50) {
-        currency -= 50;
-        countH++;
-        objExchange.H = countH;
+    
+    if (currency >= 50) {
+        objExchange.H = Math.floor(currency/50);
+        currency = currency % 50;
     }
-    while (currency >= 25) {
-        currency -= 25;
-        countQ++;
-        objExchange.Q = countQ;
+    
+    if (currency >= 25) {
+        objExchange.Q = Math.floor(currency/25);
+        currency = currency % 25;
     }
-    while (currency >= 10) {
-        currency -= 10;
-        countD++;
-        objExchange.D = countD;
+    if (currency >= 10) {
+        objExchange.D = Math.floor(currency/10);
+        currency = currency % 10;
     }
-    while (currency >= 5) {
-        currency -= 5;
-        countN++;
-        objExchange.N = countN;
+    if (currency >= 5) {
+        objExchange.N = Math.floor(currency/5);
+        currency = currency % 5;
     }
-    while (currency >= 1) {
-        currency -= 1;
-        countP++;
-        objExchange.P = countP;
+    if (currency >= 1) {
+        objExchange.P = currency;        
     }
     return objExchange;
-
 }
